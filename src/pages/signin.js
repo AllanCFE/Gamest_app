f7019a49c46d095b3e8f50b9807412bf9fab50f2
AllanCFE <allan.echeverria@gmail.com>
2023-02-09 11:34:27 -0300

    Logo Gamest

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,26 +1,33 @@
 import { getProviders, signIn, getSession } from "next-auth/react"
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/SignIn.module.css'
 import Image from "next/image"
 
+import GamestLogo from '../../public/Logos/Gamest.png'
+import loginImage from '../../public/login_img.png'
+
+/*
+Object.values(providers).map((provider) => (
+  <span className={styles.logo}>
+    <Image src="/vercel.svg" alt={provider.id} width={90} height={20} onClick={() => signIn(provider.id)}/>
+  </span>
+))
+*/
 export default function SignIn({ providers }) {
-  Object.values(providers).map((provider) => (
+  /*Object.values(providers).map((provider) => (
     console.log(provider)
-  ))
+  ))*/
   return (
     <>
       <div className={styles.holder}>
+        
         <div className={styles.left}>
-          {
-            Object.values(providers).map((provider) => (
-              <span className={styles.logo}>
-                <Image src="/vercel.svg" alt={provider.id} width={90} height={20} onClick={() => signIn(provider.id)}/>
-              </span>
-            ))
-          }
+            <div className={styles.logo}>
+                <Image src={GamestLogo} alt="logo" style={{objectFit: 'contain', maxWidth: '100%', maxHeight: '5vh'}}/>
+            </div>
         </div>
             
         <div className={styles.right}>
-            <Image src="/login_img.png" alt="login_image" fill={true}/>
+            <Image src={loginImage} alt="login_image" fill={true}/>
         </div>
       </div>
 
