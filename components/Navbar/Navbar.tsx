aeb7dc3d868fd2aba313fd3ce28975021d5bf9e1
AllanCFE <allan.echeverria@gmail.com>
2023-02-15 11:20:04 -0300

    feat: Navbar dropdown menu

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -17,13 +17,25 @@ export default function Navbar () {
                         <div className={styles.notification}></div>
                     </div>
                 </div>
-                <div className={[styles.option, styles.profilePicture].join(' ')} onClick={() => signOut()}>
-                    <Image src='https://github.com/AllanCFE.png' width={50} height={50} alt="Profile Picture" />
-                    <div className={styles.rightProfilePicture}>
-                        <div className={styles.notification}></div>
-                        <div className={styles.arrowDown}></div>
+
+                <div className={styles.dropdownArea}>
+                    <div className={[styles.option, styles.profilePicture].join(' ')} onClick={() => signOut()}>
+                        <Image src='https://github.com/AllanCFE.png' width={50} height={50} alt="Profile Picture" />
+                        <div className={styles.rightProfilePicture}>
+                            <div className={styles.notification}></div>
+                            <div className={styles.arrowDown}></div>
+                        </div>
                     </div>
+                    <ul className={styles.dropdownContent}>
+                        <li className={styles.dropdownItem}>Meu Perfil</li>
+                        <li className={styles.dropdownItem}>Perfil da Empresa</li>
+                        <li className={styles.dropdownItem}>Dashboard</li>
+                        <li className={styles.dropdownItem}>Configurações</li>
+                        <span className={[styles.horizontalBar, styles.dropdownItem].join(" ")}></span>
+                        <li className={[styles.dropdownItem, styles.exitButton].join(" ")} onClick={() => signOut()}>Sair</li>
+                    </ul>
                 </div>
+                
             </span>
         </nav>
     )
