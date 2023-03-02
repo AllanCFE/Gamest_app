92a1c3d97440a07529ee9cacdce38604c945c83c
AllanCFE <allan.echeverria@gmail.com>
2023-03-02 10:48:04 -0300

    fix: redirect to signup pages for each type of client

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import LeftImage from '../../public/signup/left.png'
 import CompanyImage from '../../public/signup/company.png'
 import UserImage from '../../public/signup/user.png'
+import Link from 'next/link'
 
 export default function signUp () {
     return (
@@ -17,22 +18,28 @@ export default function signUp () {
                     <h1>O que você está buscando?</h1>
                 </span>
                 <div className={styles.cardsArea}>
-                    <div className={styles.card}>
-                        <span>
-                            <Image src={CompanyImage} alt="signup"/>
-                        </span>
-                        <span>
-                            I’m a company looking for new talents to join my team
-                        </span>
-                    </div>
-                    <div className={styles.card}>
-                        <span>
-                            <Image src={UserImage} alt="signup"/>
-                        </span>
-                        <span>
-                            I’m a developer looking for new opportunities
-                        </span>
-                    </div>
+                    
+                    <Link href='/signup/company'>
+                        <div className={styles.card}>
+                            <span>
+                                <Image src={CompanyImage} alt="signup"/>
+                            </span>
+                            <span>
+                                I’m a company looking for new talents to join my team
+                            </span>
+                        </div>
+                    </Link>
+
+                    <Link href='/signup/user'>
+                        <div className={styles.card}>
+                            <span>
+                                <Image src={UserImage} alt="signup"/>
+                            </span>
+                            <span>
+                                I’m a developer looking for new opportunities
+                            </span>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </main>
