73b6d56fff332b5806571efd7b6a24dafc20caa1
AllanCFE <allan.echeverria@gmail.com>
2023-04-26 19:08:53 -0300

    feat: user dashboard requesting info from API

--- src/pages/user/dashboard.tsx
+++ src/pages/user/dashboard.tsx
@@ -6,12 +6,56 @@ import useRequireAuth from 'components/useRequireAuth/useRequireAuth'
 import Link from 'next/link';
 
 import DefaultAvatar from '../../../public/Default/Default_Avatar.png'
+import { useEffect, useState } from 'react';
+import { profile } from 'console';
 
 export default function Dashboard () {
 
     const session = useRequireAuth();
-    if (!session) return <div>Loading...</div>
+    
+    
+    type ProfileInfo = {
+        about: string;
+        birthday: number;
+        contact: {
+            email: string;
+            phone: string;
+        }
+        languages: string[];
+        lastUpdate: {
+            _seconds: number;
+            _nanoseconds: number;
+        };
+        programming_languages: string[];
+        workModel: string;
+        tools: string[];
+    }
+    
+    // Retrieve data from API via POST request
+    const [profileInfo, setProfileInfo] = useState<ProfileInfo | null>(null);
+    const [isLoading, setLoading] = useState(true);
 
+    useEffect(() => {
+        if(typeof session != 'undefined') {
+            setLoading(true);
+            fetch('http://127.0.0.1:5001/gamest-app/us-central1/getAllowedProfile', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ id: session?.user?.uid })
+            })
+            .then(res => res.json())
+            .then(data => {
+                setProfileInfo(data);
+                setLoading(false);
+            })
+        }
+    }, [session]);
+
+    
+    
+    if (!session) return <div>Loading...</div>
     const avatar = session.user.image ? session.user.image : DefaultAvatar;
 
     return (
@@ -19,151 +63,76 @@ export default function Dashboard () {
         <StandardBlockDivision 
             leftChildren={
                 <>
-                    <div className={styles.companyTop}>
-                        <span className={styles.companyLogo}>
-                            <Image src={avatar} alt="Company Logo" width={200} height={200} />
-                        </span>
-                        <span>
-                            <h2>Allan Echeverria</h2>
-                        </span>
-                        <span>
-                            <p className={styles.lastUpdated}>Last updated: 2022/02/28</p>
-                        </span>
-                    </div>
-                    
-                    <div className={styles.horizontalBar}></div>
+                    {isLoading ?
+                        <div>Loading...</div>
+                        :
+                        <>
+                            <div className={styles.companyTop}>
+                                <span className={styles.companyLogo}>
+                                    <Image src={avatar} alt="Company Logo" width={200} height={200} />
+                                </span>
+                                <span>
+                                    <h2>Allan Echeverria</h2>
+                                </span>
+                                <span>
+                                    <p className={styles.lastUpdated}>Last updated:
+                                    {// Convert last update to string
+                                        profileInfo?.lastUpdate != null &&
+                                        new Date(profileInfo?.lastUpdate._seconds * 1000).toLocaleDateString('en-US', {
+                                            year: 'numeric',
+                                            month: 'long',
+                                            day: 'numeric'
+                                        })
+                                    }
+                                    </p>
+                                </span>
+                            </div>
+                            
+                            <div className={styles.horizontalBar}></div>
 
-                    <div className={styles.infoArea}>
-                        <p><b>Age:</b> 23</p>
-                        <p><b>Work model:</b> Remote / Hybrid</p>
-                        <p><b>Languages:</b></p>
-                        <ul>
-                            <li>English (Native)</li>
-                            <li>Spanish (Intermediate)</li>
-                            <li>German (Basic)</li>
-                        </ul>
-                        <p><b>Contact:</b></p>
-                        <ul>
-                            <li>123456789</li>
-                            <li><Link href='mailto:example@email.com'>example@email.com</Link></li>
-                        </ul>
-                        <span 
-                        className={styles.infoAreaButton}>Edit profile</span>
-                    </div>
+                            <div className={styles.infoArea}>
+                                <p><b>Age:</b> {
+                                    // Convert birthday to age
+                                    profileInfo?.birthday != null &&
+                                    Math.floor((Date.now() - profileInfo?.birthday) / 31557600000)
+                                }</p>
+                                <p><b>Work model:</b> {profileInfo?.workModel} </p>
+                                <p><b>Languages:</b></p>
+                                <ul>
+                                    {
+                                        profileInfo?.languages != null && profileInfo?.languages.length > 0 ?                                        
+                                            profileInfo?.languages.map((language, index) => {
+                                                return <li key={index}>{language}</li>
+                                            })
+                                        :
+                                            <li>None</li>
+                                    }
+                                </ul>
+                                <p><b>Contact:</b></p>
+                                <ul>
+                                    {profileInfo?.contact != null && profileInfo?.contact.phone != null ?
+                                        <li>{profileInfo?.contact.phone}</li>
+                                        :
+                                        <li>None</li>
+                                    }
+                                    {
+                                        profileInfo?.contact != null && profileInfo?.contact.email != null ?
+                                        <li><Link href={`mailto:${profileInfo?.contact.email}`}></Link>{profileInfo?.contact.email}</li>
+                                        :
+                                        <li>None</li>
+                                    }
+                                </ul>
+                                <span 
+                                className={styles.infoAreaButton}>Edit profile</span>
+                            </div>
+                        </>}
 
                 </>
             }
             
             rightChildren={
                 <div className={styles.scrollBar}>
-
-                    <div className={styles.vacancyArea}>
-                        <div className={styles.vacancyRight}>
-                            <span className={styles.calendar}>
-                                <Image src="https://i.imgur.com/ICodYAI.png" alt="calendar" width={150} height={150} />
-                            </span>
-                            <div className={styles.vacancyDescription}>
-                                <h2>Software Developer</h2>
-                                <div className={styles.vacancyOptions}>
-                                    <div className={styles.vacancyOptionsLine}>
-                                        <p>Location: </p>
-                                        <p>Salary: </p>
-                                        <p>Experience: </p>
-                                    </div>
-                                    <div className={[styles.vacancyOptionsLine, styles.vacancyOptionValue].join(" ")}>
-                                        <p>Remote</p>
-                                        <p>$100,000</p>
-                                        <p>5 years</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={styles.vacancyLeft}>
-                            <span className={styles.editButton}>Responder</span>
-                            <span className={styles.deleteButton}>Desistir</span>
-                        </div>
-                    </div>
-
-                    <div className={styles.vacancyArea}>
-                        <div className={styles.vacancyRight}>
-                            <span className={styles.calendar}>
-                                <Image src="https://i.imgur.com/ICodYAI.png" alt="calendar" width={150} height={150} />
-                            </span>
-                            <div className={styles.vacancyDescription}>
-                                <h2>Software Developer</h2>
-                                <div className={styles.vacancyOptions}>
-                                    <div className={styles.vacancyOptionsLine}>
-                                        <p>Location: </p>
-                                        <p>Salary: </p>
-                                        <p>Experience: </p>
-                                    </div>
-                                    <div className={[styles.vacancyOptionsLine, styles.vacancyOptionValue].join(" ")}>
-                                        <p>Remote</p>
-                                        <p>$100,000</p>
-                                        <p>5 years</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={styles.vacancyLeft}>
-                            <span className={styles.waitButton}>Aguarde</span>
-                            <span className={styles.deleteButton}>Desistir</span>
-                        </div>
-                    </div>
-
-                    <div className={styles.vacancyArea}>
-                        <div className={styles.vacancyRight}>
-                            <span className={styles.calendar}>
-                                <Image src="https://i.imgur.com/ICodYAI.png" alt="calendar" width={150} height={150} />
-                            </span>
-                            <div className={styles.vacancyDescription}>
-                                <h2>Software Developer</h2>
-                                <div className={styles.vacancyOptions}>
-                                    <div className={styles.vacancyOptionsLine}>
-                                        <p>Location: </p>
-                                        <p>Salary: </p>
-                                        <p>Experience: </p>
-                                    </div>
-                                    <div className={[styles.vacancyOptionsLine, styles.vacancyOptionValue].join(" ")}>
-                                        <p>Remote</p>
-                                        <p>$100,000</p>
-                                        <p>5 years</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={styles.vacancyLeft}>
-                            <span className={styles.waitButton}>Aguarde</span>
-                            <span className={styles.deleteButton}>Desistir</span>
-                        </div>
-                    </div>
-
-                    <div className={styles.vacancyArea}>
-                        <div className={styles.vacancyRight}>
-                            <span className={styles.calendar}>
-                                <Image src="https://i.imgur.com/ICodYAI.png" alt="calendar" width={150} height={150} />
-                            </span>
-                            <div className={styles.vacancyDescription}>
-                                <h2>Software Developer</h2>
-                                <div className={styles.vacancyOptions}>
-                                    <div className={styles.vacancyOptionsLine}>
-                                        <p>Location: </p>
-                                        <p>Salary: </p>
-                                        <p>Experience: </p>
-                                    </div>
-                                    <div className={[styles.vacancyOptionsLine, styles.vacancyOptionValue].join(" ")}>
-                                        <p>Remote</p>
-                                        <p>$100,000</p>
-                                        <p>5 years</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={styles.vacancyLeft}>
-                            <span className={styles.waitButton}>Aguarde</span>
-                            <span className={styles.deleteButton}>Desistir</span>
-                        </div>
-                    </div>
+                    COMING SOON
                 </div>
             }
         />
